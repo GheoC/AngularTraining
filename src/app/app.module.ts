d2edce0a43fd3cdd6ddd35a40231eed7385b07f9
GheorgheCoanta <gheorghecoanta@coherentsolutions.com>
2023-02-24 12:19:56 +0200

    Angular Day-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageOneComponent } from './components/page-one/page-one.component';
+import { PageTwoComponent } from './components/page-two/page-two.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCardModule} from '@angular/material/card';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatChipsModule} from '@angular/material/chips';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageOneComponent,
+    PageTwoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatButtonToggleModule,
+    MatDividerModule,
+    MatChipsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/page-one/page-one.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-one',
+  templateUrl: './page-one.component.html',
+  styleUrls: ['./page-one.component.css']
+})
+export class PageOneComponent {
+
+  title:String = "Page One"
+
+}
